a45f513fc354b6ba42e0366d38660287af37b744
ArshikAk <arshik0404@gmail.com>
2024-11-19 11:50:36 +0530

    final commit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,15 @@
 /* eslint-disable react/prop-types */
 import { CiLogout } from "react-icons/ci";
+import { useAuth } from "../context/AuthContext";
 
 const Navbar = ({toggleProfileSection}) => {
+
+  const {logout} = useAuth()
+
   return (
     <div className="py-3 px-2 flex flex-col justify-between items-center h-full">
       <img src={"/personLogo.png"} alt="" className="rounded-full w-[35px] h-[35px] cursor-pointer" onClick={() => toggleProfileSection()} />
-      <CiLogout className="text-white cursor-pointer" size={30}/>
+      <CiLogout className="text-white cursor-pointer" size={30} onClick={() => logout()}/>
     </div>
   )
 }
